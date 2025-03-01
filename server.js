1cde4672846f8b7d91e1a5979e9b10c616b725ba
Bes4instvo <vadimvaitovich2009@gmail.com>
2025-03-01 07:24:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -102,6 +102,3 @@ app.get('/reject', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log("Сервер запущен на всех IP-адресах");
-});
